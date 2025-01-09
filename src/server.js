b874ac6cef5cd816fa8526afeb49f7e5e607e61c
dvankevich <dvankevich@gmail.com>
2025-01-09 21:14:44 +0200

    add error handlers middleware

--- src/server.js
+++ src/server.js
@@ -5,6 +5,9 @@ import pino from 'pino-http';
 import cors from 'cors';
 import { getEnvVar } from './utils/getEnvVar.js';
 import studentsRouter from './routers/students.js'; // Імпортуємо роутер
+// Імпортуємо middleware
+import { errorHandler } from './middlewares/errorHandler.js';
+import { notFoundHandler } from './middlewares/notFoundHandler.js';
 
 // Читаємо змінну оточення PORT
 const PORT = Number(getEnvVar('PORT', '3000'));
@@ -31,18 +34,9 @@ export const startServer = () => {
 
   app.use(studentsRouter); // Додаємо роутер до app як middleware
 
-  app.use('*', (req, res, next) => {
-    res.status(404).json({
-      message: 'Not found',
-    });
-  });
+  app.use('*', notFoundHandler);
 
-  app.use((err, req, res, next) => {
-    res.status(500).json({
-      message: 'Something went wrong',
-      error: err.message,
-    });
-  });
+  app.use(errorHandler);
 
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
