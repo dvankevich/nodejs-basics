4ea82f24908fe8d739759d23576843d8ed58e9db
dvankevich <dvankevich@gmail.com>
2025-01-09 19:52:24 +0200

    add controllers

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,34 +1,13 @@
-// src/routers/students.js
-
 import { Router } from 'express';
-import { getAllStudents, getStudentById } from '../services/students.js';
+
+import {
+  getStudentsController,
+  getStudentByIdController,
+} from '../controllers/students.js';
 
 const router = Router();
 
-router.get('/students', async (req, res) => {
-  const students = await getAllStudents();
-
-  res.status(200).json({
-    data: students,
-  });
-});
-
-router.get('/students/:studentId', async (req, res, next) => {
-  const { studentId } = req.params;
-  const student = await getStudentById(studentId);
-
-  // Відповідь, якщо контакт не знайдено
-  if (!student) {
-    res.status(404).json({
-      message: 'Student not found',
-    });
-    return;
-  }
-
-  // Відповідь, якщо контакт знайдено
-  res.status(200).json({
-    data: student,
-  });
-});
+router.get('/students', getStudentsController);
+router.get('/students/:studentId', getStudentByIdController);
 
 export default router;
