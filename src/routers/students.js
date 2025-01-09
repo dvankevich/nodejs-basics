4a789201a7582ce9428e1b8f6ec028aa61379805
dvankevich <dvankevich@gmail.com>
2025-01-09 20:52:48 +0200

    add wrapper

--- src/routers/students.js
+++ src/routers/students.js
@@ -1,13 +1,17 @@
+// src/routers/students.js
+
 import { Router } from 'express';
 
 import {
   getStudentsController,
   getStudentByIdController,
 } from '../controllers/students.js';
+import { ctrlWrapper } from '../utils/ctrlWrapper.js';
 
 const router = Router();
 
-router.get('/students', getStudentsController);
-router.get('/students/:studentId', getStudentByIdController);
+router.get('/students', ctrlWrapper(getStudentsController));
+
+router.get('/students/:studentId', ctrlWrapper(getStudentByIdController));
 
 export default router;
