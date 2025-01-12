dadf64f9f8864d73f428bb3fde9ad58413c21e2e
dvankevich <dvankevich@gmail.com>
2025-01-12 12:12:37 +0200

    add validation

---
+++ src/validation/students.js
@@ -0,0 +1,11 @@
+// src/validation/students.js
+
+import Joi from 'joi';
+
+export const createStudentSchema = Joi.object({
+  name: Joi.string().min(3).max(30).required(),
+  age: Joi.number().integer().min(6).max(16).required(),
+  gender: Joi.string().valid('male', 'female', 'other').required(),
+  avgMark: Joi.number().min(2).max(12).required(),
+  onDuty: Joi.boolean(),
+});
