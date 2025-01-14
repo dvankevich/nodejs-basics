6a783e70f3ef3167a6c0b96c00d215998bd2326f
dvankevich <dvankevich@gmail.com>
2025-01-14 11:03:21 +0200

    Sessions. access+refresh tokens

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,9 +1,10 @@
 // src/services/auth.js
-
+import { randomBytes } from 'crypto';
 import bcrypt from 'bcrypt';
-
 import { UsersCollection } from '../db/models/user.js';
 import createHttpError from 'http-errors';
+import { FIFTEEN_MINUTES, ONE_DAY } from '../constants/index.js';
+import { SessionsCollection } from '../db/models/session.js';
 
 export const registerUser = async (payload) => {
   const user = await UsersCollection.findOne({ email: payload.email });
@@ -28,5 +29,16 @@ export const loginUser = async (payload) => {
     throw createHttpError(401, 'Unauthorized');
   }
 
-  // далі ми доповнемо цей сервіс
+  await SessionsCollection.deleteOne({ userId: user._id });
+
+  const accessToken = randomBytes(30).toString('base64');
+  const refreshToken = randomBytes(30).toString('base64');
+
+  return await SessionsCollection.create({
+    userId: user._id,
+    accessToken,
+    refreshToken,
+    accessTokenValidUntil: new Date(Date.now() + FIFTEEN_MINUTES),
+    refreshTokenValidUntil: new Date(Date.now() + ONE_DAY),
+  });
 };
